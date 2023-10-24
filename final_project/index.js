76526ef9be574594005067a3a28bcdba51639378
mohamed-nassim <98819817+mohamed-nassim@users.noreply.github.com>
2023-10-24 15:15:16 +0200

    Update index.js
    bla3

--- final_project/index.js
+++ final_project/index.js
@@ -1,21 +1,112 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
-const session = require('express-session')
-const customer_routes = require('./router/auth_users.js').authenticated;
-const genl_routes = require('./router/general.js').general;
+let books = require("./booksdb.js");
+const regd_users = express.Router();
 
-const app = express();
+let users = [];
 
-app.use(express.json());
+const isValid = (username)=>{ //returns boolean
+//write code to check is the username is valid
+}
 
-app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
+const authenticatedUser = (username,password)=>{
+    let validusers = users.filter((user)=>{
+      return (user.username === username && user.password === password)
+    });
+    if(validusers.length > 0){
+      return true;
+    } else {
+      return false;
+    }
+  }
 
-app.use("/customer/auth/*", function auth(req,res,next){
+//only registered users can login
+regd_users.post("/login", (req,res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+  
+    if (!username || !password) {
+        return res.status(404).json({message: "Error logging in"});
+    }
+  
+    if (authenticatedUser(username,password)) {
+      let accessToken = jwt.sign({
+        data: password
+      }, 'access', { expiresIn: 60 * 60 });
+  
+      req.session.authorization = {
+        accessToken,username
+    }
+    return res.status(200).send("User successfully logged in");
+    } else {
+      return res.status(208).json({message: "Invalid Login. Check username and password"});
+    }
+  });
+
+// Add a book review
+regd_users.put("/auth/review/:isbn", (req, res) => {
+  //Write your code here
+  let isbn= req.params.isbn;
+  let review =  req.query.review;
+  if(req.session.authorization) {
+    token = req.session.authorization['accessToken'];
+    username = req.session.authorization['username'];
+    jwt.verify(token, "access",(err,user)=>{
+        if(!err){
+            req.user = user;
+            //let username = user.username;
+            //let userexsit= (books[isbn].reviews).filter((user1) => {
+               //return (user1.userame === username);
+            //});
+            let exist = false;
+            for (i in books[isbn].reviews) {
+                if(i === username){
+                    books[isbn].reviews[username] = review;
+                    exist = true;
+                    return res.status(200).send("the review is modified");
+               
+                }
+            }
+            if(!exist){
+                books[isbn].reviews[username] = review;
+                res.status(200).send(username);
+            }
+            next();
+        }
+        else{
+            return res.status(403).json({message: "User not authenticated"})
+        }
+     });
+ } else {
+     return res.status(403).json({message: "User not logged in"})
+ }
+  
+  //return res.status(300).json({message: "Yet to be implemented"});
+});
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+    let isbn= req.params.isbn;
     if(req.session.authorization) {
         token = req.session.authorization['accessToken'];
+        username = req.session.authorization['username'];
         jwt.verify(token, "access",(err,user)=>{
             if(!err){
                 req.user = user;
+                //let username = user.username;
+                //let userexsit= (books[isbn].reviews).filter((user1) => {
+                   //return (user1.userame === username);
+                //});
+                let exist = false;
+                for (i in books[isbn].reviews) {
+                    if(i === username){
+                        delete books[isbn].reviews[i];
+                        exist = true;
+                        return res.status(200).send("the review is removed");
+                   
+                    }
+                }
+                if(!exist){
+                    res.status(200).send("This user dosen't have a review");
+                }
                 next();
             }
             else{
@@ -25,11 +116,9 @@ app.use("/customer/auth/*", function auth(req,res,next){
      } else {
          return res.status(403).json({message: "User not logged in"})
      }
+    
 });
- 
-const PORT =5000;
 
-app.use("/customer", customer_routes);
-app.use("/", genl_routes);
-
-app.listen(PORT,()=>console.log("Server is running"));
+module.exports.authenticated = regd_users;
+module.exports.isValid = isValid;
+module.exports.users = users;
