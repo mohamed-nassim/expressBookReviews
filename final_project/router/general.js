e8cf58651e069f3d0d5fcf0bb2acf9437f9c4122
mohamed-nassim <98819817+mohamed-nassim@users.noreply.github.com>
2023-10-24 15:33:10 +0200

    Update general.js
    bla

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -32,32 +32,58 @@ public_users.get('/',function (req, res) {
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  res.send(books[req.params.isbn])
-  //return res.status(300).json({message: "Yet to be implemented"});
- });
-  
-// Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-    for (let step = 0; step < 9; step++) {
-        if(books[step+1].author === req.params.author){
-            res.send(books[step+1])
+let myPromise1 = new Promise((resolve,reject) => {
+    public_users.get('/',function (req, res) {
+        res.send(JSON.stringify(books));
+      });
+      
+    resolve()});
+   
+myPromise1.then((successMessage) => {
+    console.log("From Callback " + successMessage)
+    });
+let myPromise2 = new Promise((resolve,reject) => {
+    public_users.get('/isbn/:isbn',function (req, res) {
+        res.send(books[req.params.isbn])
+        //return res.status(300).json({message: "Yet to be implemented"});
+        });
+        
+    resolve()});
+myPromise2.then((successMessage) => {
+    console.log("From Callback " + successMessage)
+    });
+let myPromise3 = new Promise((resolve,reject) => {
+    // Get book details based on author
+    public_users.get('/author/:author',function (req, res) {
+        for (let step = 0; step < 9; step++) {
+            if(books[step+1].author === req.params.author){
+                res.send(books[step+1])
+            }
         }
-      }
   
   //return res.status(300).json({message: "Yet to be implemented"});
 });
-
-// Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-    for (let step = 0; step < 9; step++) {
-        if(books[step+1].title === req.params.title){
-            res.send(books[step+1])
+        
+    resolve()});
+myPromise3.then((successMessage) => {
+    console.log("From Callback " + successMessage)
+    });
+let myPromise4 = new Promise((resolve,reject) => {
+    /// Get all books based on title
+    public_users.get('/title/:title',function (req, res) {
+        for (let step = 0; step < 9; step++) {
+            if(books[step+1].title === req.params.title){
+                res.send(books[step+1])
+            }
         }
-      }
-  //return res.status(300).json({message: "Yet to be implemented"});
-});
+    //return res.status(300).json({message: "Yet to be implemented"});
+    });
+            
+    resolve()});
 
+myPromise4.then((successMessage) => {
+    console.log("From Callback " + successMessage)
+    });
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
     res.send(books[req.params.isbn].reviews)
